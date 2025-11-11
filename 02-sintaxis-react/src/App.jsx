a1c54103b261153a342b856601759a8873fe0535
Salome743 <callezapatasalome@gmail.com>
2025-11-11 13:17:28 -0500

    sección cards terminado

--- 02-sintaxis-react/src/App.jsx
+++ 02-sintaxis-react/src/App.jsx
@@ -1,64 +1,77 @@
-import "./App.css"
-import { NavBar } from './components/NavBar'
-import { ExtensionCard } from './components/ExtensionCard'
-import { LoginForm } from "./components/LoginForm"
-import ProductCard from "./components/ProductCard"
+import "./App.css";
+import { NavBar } from "./components/NavBar";
+import { ExtensionCard } from "./components/ExtensionCard";
+import { LoginForm } from "./components/LoginForm";
+import ProductCard from "./components/ProductCard";
 
 export const App = () => {
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <div className="containerCards">
-      <ExtensionCard
-        cardTitle="Google Translate"
-        cardImage="🆔"
-        cardDescription="Aplicación para traducir página"
-      />
-      <ExtensionCard
-        cardTitle="Spotify"
-        cardImage="🚀"
-        cardDescription="Aplicacion para escuchar musica"
-      />
+        <ExtensionCard
+          cardTitle="Google Translate"
+          cardImage="🆔"
+          cardDescription="Aplicación para traducir página"
+        />
+        <ExtensionCard
+          cardTitle="Spotify"
+          cardImage="🚀"
+          cardDescription="Aplicacion para escuchar musica"
+        />
 
-      <ExtensionCard 
-        cardTitle="Samsung"
-        cardImage="📱"
-        cardDescription="Aplicion de samsung"
-      />
-      <ExtensionCard
-      cardTitle="YouTube"
-      cardImage="🎬"
-      cardDescription="Aplicacion para ver videos"
-      />
-      <ExtensionCard
-      cardTitle="Instagram"
-      cardImage="📸"
-      cardDescription="Aplicacion para compartir fotos"
-      />
-      <ExtensionCard
-      cardTitle="Netflix"
-      cardImage="🎥"
-      cardDescription="Aplicacion para ver series y peliculas"
-      />
-      <ExtensionCard
-      cardTitle="Didi Food"
-      cardImage="🍔"
-      cardDescription="Aplicacion para pedir comida"
-      />
-      <ExtensionCard
-      cardTitle="Gallery"
-      cardImage="🖼"
-      cardDescription="Aplicacion para ver fotos"
-      />
-      <ExtensionCard
-      cardTitle="Tik Tok"
-      cardImage="📯"
-      cardDescription="Aplicacion para ver y grabar videos cortos"
-      />  
-      
+        <ExtensionCard
+          cardTitle="Samsung"
+          cardImage="📱"
+          cardDescription="Aplicion de samsung"
+        />
+        <ExtensionCard
+          cardTitle="YouTube"
+          cardImage="🎬"
+          cardDescription="Aplicacion para ver videos"
+        />
+        <ExtensionCard
+          cardTitle="Instagram"
+          cardImage="📸"
+          cardDescription="Aplicacion para compartir fotos"
+        />
+        <ExtensionCard
+          cardTitle="Netflix"
+          cardImage="🎥"
+          cardDescription="Aplicacion para ver series y peliculas"
+        />
+        <ExtensionCard
+          cardTitle="Didi Food"
+          cardImage="🍔"
+          cardDescription="Aplicacion para pedir comida"
+        />
+        <ExtensionCard
+          cardTitle="Gallery"
+          cardImage="🖼"
+          cardDescription="Aplicacion para ver fotos"
+        />
+        <ExtensionCard
+          cardTitle="Tik Tok"
+          cardImage="📯"
+          cardDescription="Aplicacion para ver y grabar videos cortos"
+        />
       </div>
-      <LoginForm/>
-      <ProductCard/>
+      <LoginForm />
+
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 col-lg-4">
+            <ProductCard />
+          </div>
+          <div className="col-12 col-md-6 col-lg-4 mt-4 mt-md-0">
+            <ProductCard />
+          </div>
+          <div className="col-12 col-md-12 col-lg-4 mt-4 mt-lg-0">
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+
     </>
-  )
-}
+  );
+};
